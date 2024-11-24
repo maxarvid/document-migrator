9734ff99e1a07a99157e8a264a0c7175c60dade3
maxarvid <max@klimato.se>
2024-11-24 11:46:02 +0100

    Cleans up some scaffolded code

--- src/App.vue
+++ src/App.vue
@@ -3,26 +3,40 @@ import { ref } from 'vue';
 import { invoke } from '@tauri-apps/api/core';
 
 const greetMsg = ref('');
-const name = ref('');
+const connectionString = ref('');
 
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-  greetMsg.value = await invoke('greet', { name: name.value });
+async function connect() {
+  greetMsg.value = 'You tried to connect!';
+  await invoke('connect', { connectionString: connectionString.value });
 }
 </script>
 
 <template>
-  <main class="bg-slate-800 text-white">
-    <h1>Welcome to Document Migrator</h1>
+  <main class="min-h-screen w-full bg-slate-800 text-white">
+    <div class="container mx-auto">
+      <h1 class="text-lg">Document Migrator</h1>
+      <p class="text-sm">
+        Your friendly neighborhood data migrator that let's you live your best Postgres life.
+      </p>
+    </div>
 
-    <p>Your friendly neighborhood data migrator that let's you live your best Postgres life.</p>
-
-    <form @submit.prevent="greet">
-      <input v-model="name" placeholder="Enter a name..." />
-      <button type="submit">Greet</button>
+    <form @submit.prevent="connect" class="container mx-auto mt-4">
+      <label for="connectionString" class="block text-sm font-medium text-gray-300"
+        >Connection String:</label
+      >
+      <input
+        id="connectionString"
+        v-model="connectionString"
+        placeholder="mongodb://localhost:27017..."
+        class="mt-1 block w-full px-3 py-2 bg-gray-700 text-white placeholder-gray-400 border border-gray-600 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+      />
+      <button
+        type="submit"
+        class="mt-3 px-4 py-2 bg-indigo-600 text-white font-semibold rounded-md shadow hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+      >
+        Connect
+      </button>
     </form>
     <p>{{ greetMsg }}</p>
   </main>
 </template>
-
-<style scoped></style>
